9772c9701ce6542c75322e23104a408e8a4801e2
Zwukii <alen.smlatic2002@gmail.com>
2026-01-02 21:58:33 +0100

    Added def of finnhub json printout

---
+++ StockPriceViewer/src/main/java/Market/client/FinnhubQuoteResponse.java
@@ -0,0 +1,22 @@
+package Market.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class FinnhubQuoteResponse {
+    @JsonProperty("c")
+    private BigDecimal currentPrice;
+    @JsonProperty("h")
+    private BigDecimal high;
+    @JsonProperty("l")
+    private BigDecimal low;
+    @JsonProperty("o")
+    private BigDecimal open;
+    @JsonProperty("pc")
+    private BigDecimal previousClose;
+}
